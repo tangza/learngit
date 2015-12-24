db44aa68608f7c68317fd5d03b40c8e385e556e9
tangza <cabernaster@sina.com>
2015-12-25 00:57:26 +0800

    [python] update python files fix encoding problem

--- python/pythonControlPC/useos.py
+++ python/pythonControlPC/useos.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+import sys
 
 def testOS():
 	os.system('ping www.baidu.com')
@@ -13,8 +14,31 @@ def testCmdFromFile():
 	f.close()
 
 def testSubprocess():
-	p = subprocess.Popen('dir', shell=True)
-	p.wait()
+	# reload(sys)
+	# sys.setdefaultencoding('utf-8')
+
+	# os.system('set PYTHONIOENCODING=utf8')
+	print(sys.getdefaultencoding())
+	ioencoding = sys.stdout.encoding
+	print(sys.stdout.encoding)
+	try:
+		p = subprocess.Popen('dir', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		# while 1:
+		# 	buff = p.stdout.readline()
+		# 	print(buff)
+		# 	if buff == '' and p.poll() != None:
+		# 		break
+		# print(p.stdout.read())
+		# print(p.communicate())
+		infos = p.stdout.readlines()
+		# print(infos)
+		for i in infos:
+			print(i.decode(ioencoding))
+		# unicode_text = u''.join(infos)
+		# print(unicode_text)
+		p.wait()
+	except Exception as _ex:
+		print('ERROR: %s' % str(_ex))
 
 # testOS()
 # testCmdFromFile()
--- test.py
+++ test.py
@@ -5,11 +5,12 @@ def listDirectory(folder):
 	for fname in os.listdir(folder):
 		print(fname)
 
-listDirectory('.')
+# listDirectory('.')
 
 def test():
 	s = '中文'
 	print(s.encode('utf-8'))
+	print(s)
 	s1 = u'统一编码'
 	print(s1)
-# test()
+test()
